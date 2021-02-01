f42eea8c6bde0445499dff85ed6dfe77ea6329ea
PPee <puckeyp@gmail.com>
2021-02-01 14:51:24 +0000

    commit

--- Certification/SSCerts.sh
+++ Certification/SSCerts.sh
@@ -1,1 +1,9 @@
 
+sudo openssl req    -x509 \
+                    -nodes \
+                    -days 365 \
+                    -newkey \
+                    rsa:2048 \
+                    -keyout localhost.key \
+                    -out localhost.crt \
+                    -config localhost.conf
