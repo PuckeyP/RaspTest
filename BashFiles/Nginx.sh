9dd2b31daf2de060997b96573a8ff86ebadef293
PPee <puckeyp@gmail.com>
2021-02-01 18:26:04 +0000

    commit

--- BashFiles/Nginx.sh
+++ BashFiles/Nginx.sh
@@ -1,8 +1,8 @@
 sudo docker pull nginx
-sudo docker run --restart always -d \
+sudo docker run --restart always \
+                -d nginx \
                 -p 80:80 \
                 -p 443:443 \
-
-                -v /home/pi/Nginx/Config:/etc/nginx/conf \
-                -v /home/pi/Nginx/Content:/usr/share/nginx/html \
-                -v /home/pi/Nginx/Certs:/etc/nginx/certs
+                -v /home/pi/RaspTest/Nginx/Config:/etc/nginx/conf \
+                -v /home/pi/RaspTest/Nginx/Content:/usr/share/nginx/html \
+                -v /home/pi/RaspTest/Nginx/Certs:/etc/nginx/certs
