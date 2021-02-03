6ff9a8243dd3d502fc9c5f8528264c893b49571d
PPee <puckeyp@gmail.com>
2021-02-03 12:26:23 +0000

    commit all

--- BashFiles/Nginx.sh
+++ BashFiles/Nginx.sh
@@ -2,10 +2,10 @@
 sudo docker run \
     --restart always \
 	--name Nginx \
-	--mount type=bind,source=/home/pi/RaspTest/Nginx/Content,target=/usr/share/nginx/html,readonly \
-	--mount type=bind,source=/home/pi/RaspTest/Nginx/Config,target=/etc/nginx/conf.d \
-	--mount type=bind,source=/home/pi/RaspTest/Nginx/Certs,target=/etc/ssl/private,readonly \
-	--mount type=bind,source=/home/pi/RaspTest/Nginx/Certs,target=/etc/nginx/certs,readonly \
+	--mount type=bind,source=/home/pi/RaspTest/Nginx/Content,target=/usr/share/nginx/html \
+	--mount type=bind,source=/home/pi/RaspTest/Nginx/Config/nginx.conf,target=/etc/nginx/nginx.conf \
+	--mount type=bind,source=/home/pi/RaspTest/Nginx/Certs,target=/etc/nginx/certs \
+	--mount type=bind,source=/home/pi/RaspTest/Nginx/Log,target=/var/log/nginx \
 	-p 80:80 \
 	-p 443:443 \
 	-d nginx
@@ -13,3 +13,4 @@ sudo docker run \
 
 #	--mount type=bind,source=/home/pi/RaspTest/Nginx/Certs,target=/etc/nginx/certs,readonly \
 #	--mount type=bind,source=/home/pi/RaspTest/Nginx/Config/nginx.conf,target=/etc/nginx/conf/nginx.conf,readonly \
+#	--mount type=bind,source=/home/pi/RaspTest/Nginx/Certs,target=/etc/ssl/private,readonly \
