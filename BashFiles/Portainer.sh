2f9d5838bbb0870e9d482e1ecdaccd72ac7510f1
PPee <puckeyp@gmail.com>
2021-02-16 21:20:45 +0100

    Delete Portainer.sh

--- BashFiles/Portainer.sh
+++
@@ -1,6 +0,0 @@
-sudo docker pull portainer/portainer-ce
-sudo docker run --restart always -d \
-                -p 9000:9000 \
-                -v /var/run/docker.sock:/var/run/docker.sock \
-                -v portainer_data:/data \
-                portainer/portainer-ce
